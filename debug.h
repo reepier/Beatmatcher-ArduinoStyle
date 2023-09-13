cdc44db55a15dc52e3fc572726b01cfd194d7a28
reepier <pierre.vezin98@gmail.com>
2023-09-13 20:25:45 +0200

    repo reordered in folder for arduino compatibilty

--- debug.h
+++
@@ -1,7 +0,0 @@
-/*  
-    Uncomment the following line to compile a DEBUG version (no DMX Rx function,
-    Serial plotter activated). Comment out the following line to reverse to
-    "NORMAL MODE" (DMX Rx activated, Serial plotter unavailabe)
-*/
-// #define PRINTDEBUG
-// #define FAKEMUSIC
